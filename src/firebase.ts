a7adab8c33ebc61b0229b1a7629f8f80975ec85b
ANDREYDEN <andrey2850@gmail.com>
2020-11-17 19:05:09 -0500

    Updated firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,5 +3,6 @@ import serviceAccount from './service-account.json'
 
 firebase.initializeApp(serviceAccount)
 console.log('App initialized');
+// firebase.auth().useEmulator('http://localhost:4000') // TODO: figure out why this causes CORS problems
 
 export default firebase
